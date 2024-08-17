33698a9184fc79df8c800459c06df4b6b4deb327
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-08-17 16:01:34 +0330

    create API view for gateway

--- gateway/views.py
+++ gateway/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import TransactionSerializer
+import uuid
+from rest_framework.response import Response
+from rest_framework import status
+from .models import BankAccount
 
 # Create your views here.
 
 
+class TransactionCreate(APIView):
+    def post(self, request):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            transaction = serializer.save(tranacyion_id=str(uuid.uuid4()))
+            if self.process_payment(transaction):
+                transaction.status = 'success'
+                transaction.save()
+                return Response({'transaction_id': transaction.transaction_id, 'status': 'success'},
+                                status=status.HTTP_200_OK)
+            else:
+                transaction.status = 'failed'
+                transaction.save()
+                return Response({'transaction_id': transaction.transaction_id, 'status': 'failed'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def process_payment(self, transaction):
+        try:
+            account = BankAccount.objects.get(user=transaction.user)
+            if int(account.balance) >= int(transaction.amount):
+                account.balance = str(int(account.balance) - int(transaction.amount))
+                account.save()
+                return True
+            return False
+        except BankAccount.DoesNotExist:
+            return False
--- market/views.py
+++ market/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import CartItem
 from .forms import AddToCartForm
+import requests
 
 # Create your views here.
 
@@ -29,3 +30,38 @@ def cart_view(request):
     }
 
     return render(request, 'market/cart.html', context)
+
+
+def checkout_view(request):
+    if request.method == 'POST':
+        cart_items = CartItem.objects.filter(user=request.user)
+        total_price = sum(item.total_price() for item in cart_items)
+
+        payment_data = {
+            'user': request.user.id,
+            'from_market': 'market A',
+            'amount': total_price,
+        }
+
+        response = requests.post('http://gateway_app/payment/', json=payment_data)
+
+        if response.status_code == 200:
+            CartItem.objects.filter(user=request.user).delete()
+            return redirect('success_page')
+        else:
+            return redirect('failure_page')
+
+    return render(request, 'market/')
+
+
+
+
+
+
+
+
+
+
+
+
+
