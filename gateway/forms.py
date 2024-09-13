13dd5a1e39bc5cc18e0f06e9cbf77aafe1974320
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-09-13 16:57:06 +0330

    prototype PSP views without DRF

--- gateway/forms.py
+++ gateway/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class BankAccountInfo(forms.Form):
-    account_number = forms.CharField(max_length=16, required=True)
+    account_number = forms.CharField(max_length=16, min_length=16, required=True)
     cvv2 = forms.CharField(max_length=4, required=True)
-    password = forms.CharField(max_length=128, required=True)
+    password = forms.CharField(max_length=128,  required=True, widget=forms.PasswordInput)
     decryption = forms.CharField(widget=forms.Textarea, required=False)
--- gateway/views.py
+++ gateway/views.py
@@ -1,18 +1,136 @@
-# from django.shortcuts import render
-# from rest_framework.views import APIView
-# from .serializers import TransactionSerializer
-# import uuid
-# from rest_framework.response import Response
-# from rest_framework import status
-# import requests
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.authentication import TokenAuthentication
-#
-# # Create your views here.
+from django.shortcuts import render, redirect
+from rest_framework.views import APIView
+from .serializers import TransactionSerializer
+import uuid
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from .forms import BankAccountInfo
+
+# Create your views here.
+
+
+def payment_page_view(request):
+    user = request.session.get('user')
+    product_name = request.session.get('product_name')
+    total_price = request.session.get('total_price_cart')
+
+    form = BankAccountInfo()
+
+    context = {
+        'user': user,
+        'product_name': product_name,
+        'total_price': total_price,
+        'form': form,
+    }
+
+    return render(request, 'gateway/payment_page.html', context)
+
+
+def process_payment_view(request):
+    if request.method == 'POST':
+        form = BankAccountInfo(request.POST)
+
+        if form.is_valid():
+            account_number = form.cleaned_data['account_number']
+            cvv2 = form.cleaned_data['cvv2']
+            password = form.cleaned_data['password']
+
+            user = request.session.get('user')
+            total_price = request.session.get('total_price_cart')
+
+            data_to_send = {
+                'user': user,
+                'total_price': total_price,
+                'account_number': account_number,
+                'cvv2': cvv2,
+                'password': password,
+            }
+
+            response = requests.post('http://server/process_payment', json=data_to_send)
+
+            if response.status_code == 200:
+                result = response.json().get('result')
+                request.session['transaction_result'] = result
+                return redirect('show transaction result')
+            else:
+                return render(request, 'gateway/error.html', {'error': 'خطا در ارتباط با بانک'})
+
+        else:
+            form = BankAccountInfo()
+
+        return render(request, 'gateway/payment_page.html', {'form': form})
+
+
+def show_bank_result_view(request):
+    result = request.session.get('transaction_result')
+    if result == 'success':
+        message = 'تراکنش با موفقیت انجام شد'
+    else:
+        message = 'تراکنشئ رد شد'
+
+    return render(request, 'gateway/show_result.html', {'message': message})
+
+
+def return_to_market_view(request):
+    user = request.session.get('user')
+    transaction_result = request.session.get('transaction_result')
+
+    date_to_send = {
+        'user': user,
+        'transaction_result': transaction_result,
+    }
+
+    response = requests.post('http://server-market/update_transaction/', json=date_to_send)
+
+    if response.status_code == 200:
+        return redirect('http://server-market/cart/')
+    else:
+        return render(request, 'gateway/error.html', {'error': 'خطا در بازگشت به فروشگاه'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create your views here.
 #
 #
 # class TransactionCreate(APIView):
-#     # authentication_classes = [TokenAuthentication]
+    # authentication_classes = [TokenAuthentication]
 #     # permission_classes = [IsAuthenticated]
 #
 #     def post(self, request):
